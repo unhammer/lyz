7da09140b869e5e9944762783a9d687dda77ace4
Kevin Brubeck Unhammer <pixiemotion@gmail.com>
2010-09-18 10:17:01 +0200

    settings was buggy, use same index for translators as menuitems, but set hidden=true on the non-bib ones

--- chrome/content/lyz/settings.js
+++ chrome/content/lyz/settings.js
@@ -6,23 +6,23 @@ function onLoad() {
     
     var formatMenu = document.getElementById("format-menu");
     var formatPopup = document.getElementById("format-popup");
+    var defaultIndex = 0;
 
     // add styles to format popup
-    var index = 0;
-    for(var t in translators) {
-	if(translators[t].target == "bib") {
-    	    var itemNode = document.createElement("menuitem");
-    	    itemNode.setAttribute("label", translators[t].label);
-    	    formatPopup.appendChild(itemNode);
-	    
-	    // select last selected translator
-	    if(translators[t].translatorID == window.arguments[0].inn.selectedTranslator) {
-		formatMenu.selectedIndex = index;
-	    }
-	    if(translators[t].translatorID == '9cb70025-a888-4a29-a210-93ec52da40d4') {
-		defaultIndex = index; // plain BibTeX
-	    }
-	    index++;
+    for(var i in translators) {
+    	var itemNode = document.createElement("menuitem");
+    	itemNode.setAttribute("label", translators[i].label);
+    	formatPopup.appendChild(itemNode);
+	
+	// select last selected translator
+	if(translators[i].translatorID == window.arguments[0].inn.selectedTranslator) {
+	    formatMenu.selectedIndex = i;
+	}
+	if(translators[i].translatorID == '9cb70025-a888-4a29-a210-93ec52da40d4') {
+	    defaultIndex = i; // plain BibTeX
+	}
+	if(translators[i].target != "bib") {
+    	    itemNode.setAttribute("hidden", true);
 	}
     }
     // select plain BibTeX as default:
